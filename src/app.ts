1e834214fbcc53c3983e724fe59983ab909f4fb9
Great <ezennagreat@gmail.com>
2025-10-23 16:36:38 +0100

    chore: refactor app.ts to remove unused database connection and update tsconfig.json for module and type simplification

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,12 @@
-import express, { type Request, type Response } from "express";
-import dotenv from "dotenv"
-import database from "./db.ts"
+import express, { type Request, type Response } from 'express';
+import dotenv from 'dotenv'; 
 
 dotenv.config({
     path: '.env',
 });
 
 const app = express();
-app.use(express.json())
-
-const client = await database.connect()
-
-const response = await client.query("SELECT NOW()") 
-
-app.get("/now", (req: Request, res: Response) => {
-    res.status(200).json(response.rows)
-})
+app.use(express.json());
 
 app.listen(process.env.APP_PORT, () => {
     console.log('app started @', process.env.APP_PORT);
