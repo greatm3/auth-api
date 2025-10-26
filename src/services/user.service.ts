99594f8ea01c20674d19b07ede6ee96c46a4968b
Great <ezennagreat@gmail.com>
2025-10-26 21:52:39 +0100

    feat: implemented the findAll method in UserService

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,18 @@
 import appPool from '../db';
+import { User, UserServiceType } from "../types/user.type"
 
-export class UserService {
-    constructor() {}
-    async name(params: string) {}
+export class UserService implements UserServiceType<User> {
+    constructor(private readonly db = appPool) {}
+    
+    async findAll(): Promise<User[] | Error | undefined> {
+        try {
+            const users = await this.db.query('SELECT * FROM users')
+            return users.rows
+        } catch (err) {
+            if (err instanceof Error) {
+                console.error(err.stack, 'Error fetching users from database')
+                throw new Error('Failed to fetch users from database')
+            }
+        }
+    }
 }
