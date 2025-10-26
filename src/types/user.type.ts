5b83d322ad5cdaa11c6a61771c5d8ce7cb8586fa
Great <ezennagreat@gmail.com>
2025-10-26 21:17:29 +0100

    feat: created the UserService interface in types/

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -3,3 +3,11 @@ export interface User {
     email: string;
     created_at: string;
 }
+
+export interface UserService<T> {
+    findAll(): Promise<T[]>,
+    findById(): Promise<T | null>,
+    findByEmail(): Promise<T | null>,
+    createUser(email: string, hashedPassword: string): Promise<T | null>,
+    deleteUser(email: string): Promise<T | null>
+}
