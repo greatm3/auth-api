cb4808c11e66a0905d59ebb2f1740f5d5b1eb84a
Great <ezennagreat@gmail.com>
2025-10-27 10:44:45 +0100

    feat: implemented the findById and the findByEmail async methods

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,17 +1,41 @@
 import appPool from '../db';
-import { User, UserServiceType } from "../types/user.type"
+import { User, UserServiceType } from '../types/user.type';
 
 export class UserService implements UserServiceType<User> {
     constructor(private readonly db = appPool) {}
-    
+
     async findAll(): Promise<User[] | Error | undefined> {
         try {
-            const users = await this.db.query('SELECT * FROM users')
-            return users.rows
+            const users = await this.db.query('SELECT * FROM users');
+            return users.rows;
         } catch (err) {
             if (err instanceof Error) {
-                console.error(err.stack, 'Error fetching users from database')
-                throw new Error('Failed to fetch users from database')
+                console.error(err.stack, 'Error fetching users from database');
+                throw new Error('Failed to fetch users from database');
+            }
+        }
+    }
+
+    async findById(id: number): Promise<User | undefined> {
+        try {
+            const user = await this.db.query('SELECT * FROM users WHERE id = $1', [id])
+            return user.rows[0]
+        } catch (err) {
+            if (err instanceof Error) {
+                console.log(err.stack, 'Error fetching user from database')
+                throw new Error('Failed to fetch user from database')
+            }
+        }
+    }
+
+    async findByEmail(email: string): Promise<User | undefined> {
+        try {
+            const user = await this.db.query('SELECT * FROM users WHERE email = $1', [email])
+            return user.rows[0]
+        } catch (err) {
+            if (err instanceof Error) {
+                console.log(err.stack, 'Error fetching user from database')
+                throw new Error('Failed to fetch user from database')
             }
         }
     }
