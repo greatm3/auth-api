cb9ea3a580da9009da09e0ffe4ca021861f955e7
Great <ezennagreat@gmail.com>
2025-10-27 16:49:08 +0100

    feat: making use of userService, and creation of an error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv'; 
 import cors from "cors"
 import { authRouter } from './routes/auth.route';
-import dotenv from 'dotenv'; 
+import { errorHandler } from './middlewares/error_handler.middleware'
 
 dotenv.config({
     path: '.env',
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(cors())
 
 app.use('/api/auth', authRouter)
+
+app.use(errorHandler)
 
 app.listen(process.env.APP_PORT, () => {
     console.log('app started @', process.env.APP_PORT);
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,16 +1,19 @@
-import { type Request, type Response } from 'express';
+import { type Request, type Response, type NextFunction } from 'express';
 import { validatePostRequest } from '../utils/validation.util';
 import { hashPassword } from '../utils/hash.util';
+import { UserService } from '../services/user.service';
+import appPool from '../db';
 
-async function register(req: Request, res: Response) {
+const userService = new UserService(appPool);
+
+async function register(req: Request, res: Response, next: NextFunction) {
     if (!req.body || Object.entries(req.body).length === 0) {
         return res
             .status(400)
             .json({ success: false, error: 'Email and password are required' });
     }
 
-    const email = req.body.email;
-    const password = req.body.password;
+    const { email, password } = req.body;
 
     const validationResult = validatePostRequest(email, password);
 
@@ -30,7 +33,17 @@ async function register(req: Request, res: Response) {
             .json({ success: false, error: 'Internal server error' });
     }
 
-    res.status(201).json({ res: passwordHash });
+    try {
+        if (typeof passwordHash === 'string') {
+            await userService.createUser(email, passwordHash);
+        }
+        return res.status(201).json({
+            success: true,
+            message: 'Registration successful',
+        });
+    } catch (err) {
+        next(err);
+    }
 }
 
 export { register };
