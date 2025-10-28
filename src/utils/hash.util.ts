d7de784e72d57964def3c979c81b97ec0263b7c9
Great <ezennagreat@gmail.com>
2025-10-28 11:52:31 +0100

    feat: implemented the verifyHash function in hash.util.ts

--- src/utils/hash.util.ts
+++ src/utils/hash.util.ts
@@ -2,10 +2,21 @@ import bcrypt from "bcrypt";
 
 const saltRounds = 10;
 
-export async function hashPassword(password: string): Promise<string | Error | undefined> {
+export async function hashPassword(password: string) : Promise<string | Error | undefined> {
     try {
         const hash = await bcrypt.hash(password, saltRounds);
         return hash
+    } catch (err) {
+        if (err instanceof Error) {
+            return err;
+        }
+    }
+}
+
+export async function verifyHash(hash: string, password: string) {
+    try {
+        const verified = await bcrypt.compare(hash, password);
+        return verified
     } catch (err) {
         if (err instanceof Error) {
             return err;
