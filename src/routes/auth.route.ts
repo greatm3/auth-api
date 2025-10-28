66a72ed9a18ee318836e68b67157f233c71f1f43
Great <ezennagreat@gmail.com>
2025-10-28 13:02:46 +0100

    chore(auth.route): added the /profile endpoint and handler

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { register, login } from '../controllers/auth.controller';
+import { register, login, profile } from '../controllers/auth.controller';
 
 const authRouter = Router();
 
 authRouter.post('/register', register);
 authRouter.post('/login', login)
+authRouter.get('/profile', profile)
 
 export { authRouter } 
