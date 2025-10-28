c1c2f339c6602fa0c1d2df173a26f7f1241e4c05
Great <ezennagreat@gmail.com>
2025-10-28 12:47:25 +0100

    feat(auth.login): created, tested the login controller function, create token and verify hash

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,10 @@
 import { type Request, type Response, type NextFunction } from 'express';
 import { validatePostRequest } from '../utils/validation.util';
-import { hashPassword } from '../utils/hash.util';
+import { hashPassword, verifyHash } from '../utils/hash.util';
 import { UserService } from '../services/user.service';
 import appPool from '../db';
+import { success } from 'zod';
+import { generateToken } from '../utils/jwt.util';
 
 const userService = new UserService(appPool);
 
@@ -36,17 +38,24 @@ export async function register(
     }
 
     try {
-        if (typeof passwordHash === 'string') {
+        if (passwordHash) {
             await userService.createUser(email, passwordHash);
         }
 
         const newUser = await userService.findByEmail(email);
+
         if (newUser) {
+            const userPayload = {
+                id: newUser.id,
+                email: newUser.email,
+                created_at: newUser.created_at,
+            };
+
             const response = {
                 success: true,
                 message: 'User registered succesfully',
                 data: {
-                    user: newUser,
+                    user: userPayload,
                 },
             };
             return res.status(201).json(response);
@@ -72,6 +81,52 @@ export async function login(req: Request, res: Response, next: NextFunction) {
             success: false,
             error: 'Invalid credentials',
         };
-        return res.status(422).json(response);
+        return res.status(401).json(response);
+    }
+
+    const user = await userService.findByEmail(email);
+
+    if (user) {
+        const isPasswordVerified = await verifyHash(
+            user.password_hash,
+            password
+        );
+
+        if (!isPasswordVerified) {
+            const response = {
+                success: false,
+                error: 'Invalid credentials',
+            };
+            return res.status(401).json(response);
+        }
+
+        const payload = {
+            id: user.id,
+            email: user.email,
+        };
+
+        const token = await generateToken(payload);
+
+        if (token) {
+            const response = {
+                success: true,
+                message: 'Login successful',
+                data: {
+                    token: token,
+                    user: {
+                        id: user.id,
+                        email: user.email,
+                    },
+                },
+            };
+
+            return res.status(200).json(response);
+        }
+    } else {
+        const response = {
+            success: false,
+            error: 'Invalid credentials',
+        };
+        return res.status(401).json(response);
     }
 }
